e82b1a5657763282f97a9ad6fb135f6a3a907c0b
strazds <strazds@gmail.com>
2020-04-02 19:58:08 +0200

    Updates

---
+++ precache-manifest.9d5f21d5bc097555ecc8b42cfe07aebf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130863c8dbbe5311d33c802da98b5adb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2b880e3c034ed0e039f5",
+    "url": "./static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "bc080c0c3991343dae2c",
+    "url": "./static/css/main.ec5812d7.chunk.css"
+  },
+  {
+    "revision": "2b880e3c034ed0e039f5",
+    "url": "./static/js/2.1bef0ab1.chunk.js"
+  },
+  {
+    "revision": "1a2f2540a78b2022a70889f2de7372f4",
+    "url": "./static/js/2.1bef0ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc080c0c3991343dae2c",
+    "url": "./static/js/main.064c5b82.chunk.js"
+  },
+  {
+    "revision": "595d680bce149d788ec7",
+    "url": "./static/js/runtime-main.6d35336c.js"
+  }
+]);
